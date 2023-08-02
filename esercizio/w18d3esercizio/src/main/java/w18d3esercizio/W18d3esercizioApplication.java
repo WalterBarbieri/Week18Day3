d1ab6ac6dedffe706d0c7890f098e577814c7889
Walter Barbieri <walter90@hotmail.it>
2023-08-02 18:16:40 +0200

    Fatto Set Esercizio

---
+++ esercizio/w18d3esercizio/src/main/java/w18d3esercizio/W18d3esercizioApplication.java
@@ -0,0 +1,13 @@
+package w18d3esercizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W18d3esercizioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W18d3esercizioApplication.class, args);
+	}
+
+}
---
+++ esercizio/w18d3esercizio/src/test/java/w18d3esercizio/W18d3esercizioApplicationTests.java
@@ -0,0 +1,13 @@
+package w18d3esercizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W18d3esercizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
