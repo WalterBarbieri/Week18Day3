79dbed7a18dd93be4fa51594705dc74b4f856fae
Walter Barbieri <walter90@hotmail.it>
2023-08-02 15:28:11 +0200

    Primo Commit prove mattina con env.properties in gitignore

---
+++ proveMattina/w18d3proveMattina/src/main/java/w18d3proveMattina/W18d3proveMattinaApplication.java
@@ -0,0 +1,13 @@
+package w18d3proveMattina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W18d3proveMattinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W18d3proveMattinaApplication.class, args);
+	}
+
+}
---
+++ proveMattina/w18d3proveMattina/src/test/java/w18d3proveMattina/W18d3proveMattinaApplicationTests.java
@@ -0,0 +1,13 @@
+package w18d3proveMattina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W18d3proveMattinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
